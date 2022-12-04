2d3caff3e85b5120738ff7e937621feb9322a3f5
polltter <miguelr.silva@outlook.pt>
2022-12-04 19:01:38 +0000

    merda para isto

--- RV/CruiseAmerica/CruiseAmerica/spiders/CA.py
+++ RV/CruiseAmerica/CruiseAmerica/spiders/CA.py
@@ -1,10 +1,17 @@
 import scrapy
-
+from scrapy_splash import SplashRequest
+import cloudscraper
 
 class CaSpider(scrapy.Spider):
     name = 'CA'
     allowed_domains = ['www.cruiseamerica.com']
     start_urls = ['http://www.cruiseamerica.com/']
 
+    def start_requests(self):
+        url = 'https://www.cruiseamerica.com/rv-rental-locations/alabama'
+        yield SplashRequest(url=url, callback=self.parse)
+
     def parse(self, response):
-        pass
+#        scraper = cloudscraper.create_scraper(delay=10, browser='chrome')
+#        response = scraper.get(url)
+        yield {'1': response.xpath('//*[@id="AnnistonAlabama"]/text()').get()}
