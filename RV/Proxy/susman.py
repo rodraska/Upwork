590a2574f7ed6ad8f4602f84feecabd49a19d6b5
Rodrigo Reis De Carvalho <rreis-de@c1r6s6.42lisboa.com>
2022-12-06 17:18:48 +0000

    a pata

--- RV/Proxy/susman.py
+++ RV/Proxy/susman.py
@@ -2,6 +2,12 @@ from bs4 import BeautifulSoup, NavigableString, Tag
 import requests
 import pandas as pd
 
+dfc = pd.read_csv("coordinates.csv")
+
+def give_coord(zip):
+        lat = dfc[dfc.ZIP == zip].LAT.item()
+        lng = dfc[dfc.ZIP == zip].LNG.item()
+        return (lat, lng)
 
 with open('rvt.html', 'r') as html_file:
     content = html_file.read()
@@ -13,6 +19,8 @@ with open('rvt.html', 'r') as html_file:
     dealer_city = []
     dealer_state = []
     dealer_zip = []
+    dealer_lat = []
+    dealer_lng = []
 
     for dealer in dealers:
         name = dealer.find('h3').text
@@ -23,17 +31,27 @@ with open('rvt.html', 'r') as html_file:
         ziptotal = br.nextSibling.strip().replace('\n', '').split(',')
         dealer_city.append(ziptotal[0])
         dealer_state.append(ziptotal[1])
-        dealer_zip.append(ziptotal[2])
-
+        zip = ziptotal[2]
+        dealer_zip.append(zip)
+        try:
+            lat, lng = give_coord(int(zip))
+            dealer_lat.append(lat)
+            dealer_lng.append(lng)
+        except ValueError:
+            lat, lng = "Not Found", "Not Found"
+            dealer_lat.append(lat)
+            dealer_lng.append(lng)
         #print(ziptotal)
-    #print(dealer_zip)
+    #print(len(dealer_zip))
 
     data = {
         'Name': dealer_name,
         'Address': dealer_address,
         'City': dealer_city,
         'State': dealer_state,
-        'Zip': dealer_zip
+        'Zip': dealer_zip,
+        'Lat': dealer_lat,
+        'Lng': dealer_lng
     }
 
     df = pd.DataFrame(data)
